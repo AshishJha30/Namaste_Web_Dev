bce597a2cdfb4d01fe81e6c21cf0f7cfe506b66d
“AshishJha30” <ashishjha2000.aj@gmail.com>
2025-03-08 14:07:09 +0530

    Sum_Problem_Added

---
+++ Crack_Frontend_Interview/day4_sum_Pro_Prototype.js
@@ -0,0 +1,25 @@
+
+// let sum = a => b => b ? sum(a+b) : a;
+
+//     // return function (b) {
+//     //     if(b) {
+//     //         return sum(a+b);
+//     //     }
+//     //     return a;
+//     // }
+
+// // }
+
+// console.log(sum(1)(2)(3)(4)()); 
+
+// function sum(a) {
+//     return function (b) {
+//         if(b) {
+//             return sum(a+b);
+//         }
+//         return a;
+//     }
+// }
+
+// console.log(sum(1)(2)(3)(4)()); 
+
